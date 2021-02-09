f03ee852837d485103f657665396f5706ff60f2f
sidoberg <54552277+sidoberg@users.noreply.github.com>
2021-02-09 08:55:55 +0100

    serveur fini

---
+++ Game/gestionPage.js
@@ -0,0 +1,49 @@
+var gestionPage = {
+
+     envoyerLesDonnees : function(reponse, data) {
+        reponse.writeHead(200,{"Content-Type" : data.contentType});
+        reponse.write(data.pageHtml);
+        reponse.end();
+    },
+    
+    preparerLesDonnees : function(monOBj) {
+        var indexDuPoint = monOBj.pathname.indexOf(".");
+        var extension    = monOBj.pathname.substring(indexDuPoint, monOBj.pathname.length);
+        
+    
+        var data ={
+            contentType: "",
+            encodage   : "",
+            dossier    : "",
+            fichier    : monOBj.pathname.substring(1, monOBj.pathname.length),
+        };
+        
+        switch(extension){
+            case ".html" :
+                data.contentType = "text/html";
+                data.encodage    = "UTF-8";
+                data.dossier     = "html/";
+            break;
+            case ".css" :
+                data.contentType = "text/css";
+                data.dossier     = "css/";
+            break;
+            case ".js" :
+                data.contentType = "application/javascript";
+                data.dossier     = "js_client/";
+            break;
+            case ".png" : 
+                data.contentType = "image/png";
+                data.dossier     = "assets/";
+            break;
+            case ".jpg" : 
+                data.contentType = "image/jpeg";
+                data.dossier     = "assets/";
+            break;
+            default : console.log("Erreur");
+        }
+        return data;
+    }
+}
+module.exports = gestionPage;
+
--- Game/serveur.js
+++ Game/serveur.js
@@ -1,57 +1,25 @@
-var http = require("http");
-var url  = require("url");
-var fs   = require("fs");
-const { encode } = require("punycode");
+var http        = require("http");
+var url         = require("url");
+var fs          = require("fs");
+var gestionPage = require("./gestionPage");
+var serveur     = http.createServer(traitReq);
 
 const PORT = "8080";
 
-var serveur = http.createServer(traitReq);
 serveur.listen(PORT);
 
 function traitReq(requete, reponse){
-    var monOBj       = url.parse(requete.url);
-    var contentType  = "";
-    var encodage     = "";
-    var dossier      = "";
-    var indexDupoint = monOBj.pathname.indexOf(".");
-    var extension    = monOBj.pathname.substring(indexDupoint, monOBj.pathname.length);
-    var fichier      = monOBj.pathname.substring(1, monOBj.pathname.length);
+    var monOBj = url.parse(requete.url);
 
     if(monOBj.pathname === "/"){
         monOBj.pathname = "/index.html";
     }
 
-    switch(extension){
-        case ".html" :
-            contentType = "text/html";
-            encodage    = "UTF-8";
-            dossier     = "html/";
-        break;
-        case ".css" :
-            contentType = "text/css";
-            dossier     = "css/";
-        break;
-        case ".js" :
-            contentType = "application/javascript";
-            dossier     = "js_client/";
-        break;
-        case ".png" : 
-            contentType = "image/png";
-            dossier     = "assets/";
-        break;
-        case ".jpg" : 
-            contentType = "image/jpeg";
-            dossier     = "assets/";
-        break;
-        default : console.log("Erreur");
-    }
-
-
     if(monOBj.pathname !== "/favicon.ico"){
-        var pageHtml = fs.readFileSync( dossier + fichier, encodage);
-
-        reponse.writeHead(200,{"Content-Type" : contentType});
-        reponse.write(pageHtml);
-        reponse.end();
+        var dataPreparation = gestionPage.preparerLesDonnees(monOBj);
+        var data = {};
+        data.contentType = dataPreparation.contentType;
+        data.pageHtml    = fs.readFileSync( dataPreparation.dossier + dataPreparation.fichier, dataPreparation.encodage);
+        gestionPage.envoyerLesDonnees(reponse, data)
     }
 }
